22c7a13792565f4bd5022d08fdcf8c73a3414211
cristianv.ms <73314207+CristianMacha@users.noreply.github.com>
2023-11-23 09:49:19 -0500

    effects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
 		extend: {
 			fontFamily: {
 				primary: "Aeonik"
+			},
+			backgroundImage: {
+				'sample-one-desktop': "url('https://firebasestorage.googleapis.com/v0/b/dev-system2023.appspot.com/o/360%2Fservice3.webp?alt=media&token=a229d11a-e567-43cb-a82a-fd4cda28392d')"
 			}
 		},
 	},
